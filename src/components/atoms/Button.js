f8fd07e03751e2b9b35033669efb4189b0afa294
mojahid-belaman <mojahidbelaman@gmail.com>
2023-05-18 00:50:15 +0100

    update component Button

--- src/components/atoms/Button.js
+++ src/components/atoms/Button.js
@@ -1,4 +1,12 @@
 const Button = (props) => {
-    return (<button className="bg-secondary hover:bg-tertiary text-white text-xl font-bold p-2 rounded-md" type={props?.type} onClick={props?.onClick}>{props.text}</button>)
-}
-export default Button
+  return (
+    <button
+      className="bg-secondary hover:bg-tertiary text-white text-xl font-bold p-2 rounded-md"
+      type={props?.type}
+      onClick={props?.onClick}
+    >
+      {props.text}
+    </button>
+  );
+};
+export default Button;
