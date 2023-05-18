ae2279e113bee84b40207ac8ed5129e1d85cebaa
mojahid-belaman <mojahidbelaman@gmail.com>
2023-05-18 09:11:41 +0100

    add id in dummy data

--- src/library/expensesData.js
+++ src/library/expensesData.js
@@ -1,50 +1,60 @@
 export const expensesData = [
   {
+    id: 1,
     date: new Date(2021, 7, 6),
     title: "Car Insurance",
     price: 300.37,
   },
   {
+    id: 2,
     date: new Date(2021, 4, 2),
     title: "Laptop",
     price: 1000.37,
   },
   {
+    id: 3,
     date: new Date(2022, 5, 4),
     title: "Table",
     price: 200.37,
   },
   {
+    id: 4,
     date: new Date(2022, 6, 6),
     title: "Chair",
     price: 300.37,
   },
   {
+    id: 5,
     date: new Date(2022, 6, 6),
     title: "Chair",
     price: 300.37,
   },
   {
+    id: 6,
     date: new Date(2022, 6, 6),
     title: "Chair",
     price: 300.37,
   },
   {
+    id: 7,
     date: new Date(2022, 6, 6),
     title: "Chair",
     price: 300.37,
   },
   {
+    id: 8,
     date: new Date(2022, 6, 6),
     title: "Chair",
     price: 300.37,
   },
   {
+    id: 9,
     date: new Date(2022, 6, 6),
     title: "Chair",
     price: 300.37,
   },
   {
+    id: 10,
     date: new Date(2022, 6, 6),
     title: "Chair",
     price: 300.37,
