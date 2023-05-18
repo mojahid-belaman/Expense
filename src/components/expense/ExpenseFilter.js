dfd03e4e24bccdea95efc3e33fd65e9c35521817
mojahid-belaman <mojahidbelaman@gmail.com>
2023-05-18 09:11:18 +0100

    map dates

--- src/components/expense/ExpenseFilter.js
+++ src/components/expense/ExpenseFilter.js
@@ -1,21 +1,26 @@
-import React from 'react'
-import Label from '../atoms/Label'
+import React from "react";
+import Label from "../atoms/Label";
+
+const dates = [2022, 2021, 2020, 2019];
 
 const ExpenseFilter = (props) => {
-    const funcChangeFilter = (e) => {
-        props.onChangeFilter(e.target.value)
-    }
+  const funcChangeFilter = (e) => {
+    props.onChangeFilter(e.target.value);
+  };
   return (
-    <div className='flex justify-between items-center mb-5'>
-        <Label text={'Filter By Year: '}/>
-        <select className='p-2 rounded-lg outline-none border border-[#039be5] text-[#616161] font-semibold text-xl' select={props.selected} onChange={funcChangeFilter}>
-            <option value="2022">2022</option>
-            <option value="2021">2021</option>
-            <option value="2020">2020</option>
-            <option value="2019">2019</option>
-        </select>
+    <div className="flex justify-between items-center mb-5">
+      <Label text={"Filter By Year: "} />
+      <select
+        className="p-2 rounded-lg outline-none border border-[#039be5] text-[#616161] font-semibold text-xl"
+        select={props.selected}
+        onChange={funcChangeFilter}
+      >
+        {dates.map((ele, idx) => (
+          <option key={idx} value={ele}>{ele}</option>
+        ))}
+      </select>
     </div>
-  )
-}
+  );
+};
 
-export default ExpenseFilter
+export default ExpenseFilter;
