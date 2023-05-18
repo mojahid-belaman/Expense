6b061353b02a169eb068dfd848a15dc221eea14d
mojahid-belaman <mojahidbelaman@gmail.com>
2023-05-18 18:44:24 +0100

    add component ChartBar

--- src/components/chart/ChartBar.js
+++ src/components/chart/ChartBar.js
@@ -1,10 +1,18 @@
-import React from 'react'
-import './ChartBar.css'
+import React from "react";
 
-function ChartBar() {
+const ChartBar = (props) => {
+  let barFillHeight = '0%';
+  if (props.maxValue > 0)
+    barFillHeight = Math.round((props.value / props.maxValue) * 100) + "%";
+
   return (
-    <div>ChartBar</div>
-  )
-}
+    <div className="h-full w-5 flex flex-col items-center">
+      <div className="h-full w-full border-2 border-white bg-primary rounded-xl overflow-hidden flex flex-col-reverse">
+        <div className="bg-secondary w-full transition-colors" style={{height: barFillHeight}}></div>
+      </div>
+      <div className="text-white font-bold text-xl">{props.label}</div>
+    </div>
+  );
+};
 
-export default ChartBar
+export default ChartBar;
