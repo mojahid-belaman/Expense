e6b725521af1538184bbd98e560aa307a820a2ea
mojahid-belaman <mojahidbelaman@gmail.com>
2023-05-19 10:23:51 +0100

    update style component

--- src/components/chart/Chart.js
+++ src/components/chart/Chart.js
@@ -5,7 +5,7 @@ const Chart = (props) => {
   const values = props.dataPoints.map((el) => el.value);
   const totalMax = Math.max(...values);
   return (
-    <div className="p-4 mb-3 bg-tertiary h-40 rounded-lg text-center flex justify-around">
+    <div className="p-4 mb-3 bg-tertiary h-40 rounded-lg text-center flex space-x-2 md:space-x-0 justify-around">
       {props.dataPoints.map((el) => (
         <ChartBar
           key={el.label}
